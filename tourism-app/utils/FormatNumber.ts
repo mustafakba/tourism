e3e3c019becb1629fe922988e9b4069246779724
Mustafa KABA <kaba.mustafaa@gmail.com>
2024-01-15 21:25:38 +0300

    last version tourism project

--- tourism-app/utils/FormatNumber.ts
+++
@@ -1,8 +0,0 @@
-const formatNumber = (number: number): string => {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(number);
-};
-
-export default formatNumber;
